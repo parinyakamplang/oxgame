86ebf5f4fbefb4c8965766233e420617d9365777
parinyakamplang <parinya.kamplang@gmail.com>
2024-09-20 04:31:45 +0700

    univers

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import './index.css'
 import { GoogleOAuthProvider } from "@react-oauth/google"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId='517659350675-vl7d9k1u6qpqpk58q1r1b94kaco3t0ej.apps.googleusercontent.com'>
+  <GoogleOAuthProvider clientId='517659350675-vl7d9k1u6qpqpk58q1r1b94kaco3t0ej.apps.googleusercontent.com'> 
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
-  </GoogleOAuthProvider>
+    </React.StrictMode>
+  </GoogleOAuthProvider> 
 )
